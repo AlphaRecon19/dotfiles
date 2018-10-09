5df79fd6bfe27fe25ff9a4713305e88ff03b1b55
Chris Hilsdon <chris@koolserve.uk>
2018-10-09 10:26:31 +0100

    Update PHP versions

--- docker/images/php/build.sh
+++ docker/images/php/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-docker build --squash --pull -f Dockerfile -t local/php:latest .
-
 docker build --squash --pull -f Dockerfile-7.0 -t local/php:7.0 .
-
+docker build --squash --pull -f Dockerfile-7.1 -t local/php:7.1 .
+docker build --squash --pull -f Dockerfile-7.2 -t local/php:7.2 -t local/php:latest .
 docker build --squash --pull -f Dockerfile-7.3 -t local/php:7.3 .
