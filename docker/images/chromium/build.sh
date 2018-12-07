4182db73aefa2c3c3966baf364b6b12774a09d22
Chris Hilsdon <chris@koolserve.uk>
2018-12-07 15:32:38 +0000

    Update chromium dockerfile

--- docker/images/chromium/build.sh
+++ docker/images/chromium/build.sh
@@ -1,3 +1,16 @@
-#!/bin/zsh
+#!/bin/bash
 
-docker build --squash -t alpharecon19/chromium:latest .
+export LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media"
+REVISION=$(curl -s -S $LASTCHANGE_URL)
+docker build --build-arg REVISION=$REVISION --squash -t alpharecon19/chromium:latest .
+
+version="$(docker run -it alpharecon19/chromium:latest --version)"
+regex='Chromium.(.*)'
+[[ $version =~ $regex ]]
+version="${BASH_REMATCH[1]}";
+tag="alpharecon19/chromium:${version}"
+
+echo "Run the following commands to push this to the docker hub"
+echo ""
+echo "docker tag alpharecon19/chromium:latest ${tag}"
+echo "docker push ${tag}"
